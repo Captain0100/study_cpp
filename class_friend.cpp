7201d813b48aaafedfa9a87d502c051da4fa0356
Captain0100 <131696609+Captain0100@users.noreply.github.com>
2023-04-25 10:45:52 +0800

    Update class_friend.cpp

--- class_friend.cpp
+++ class_friend.cpp
@@ -1,48 +1,48 @@
 //#define _CRT_SECURE_NO_WARNINGS
 //#include<iostream>
 //#include<string>
-//using namespace std;
-//class GorgeHome;
-//class Bob;
-//void test01()
-//{
-//	Bob b;
-//	b.visitting();
-//}
-//int main()
-//{
-//	test01();
-//	return 0;
-//}
-//class GorgeHome
-//{
-//	GorgeHome();
-//	friend class Bob;
-//public:
-//	string SittingRoom;
-//private:
-//	string BedRoom;
-//};
-//GorgeHome::GorgeHome()
-//{
-//	SittingRoom = "øÕÃ¸";
-//	BedRoom = "Œ‘ “";
-//}
-//class Bob
-//{
-//	Bob();
-//public:
-//	void visitting();
-//private:
-//	
-//	GorgeHome* place;
-//};
-//Bob::Bob()
-//{
-//	place = new GorgeHome;
-//}
-//void Bob::visitting()
-//{
-//	cout << "Bob is visitting GorgeHome " << place->SittingRoom << endl;
-//	cout << "Bob is visitting GorgeHome " << place->BedRoom << endl;
-//}//
+using namespace std;
+class GorgeHome;
+class Bob;
+void test01()
+{
+	Bob b;
+	b.visitting();
+}
+int main()
+{
+	test01();
+	return 0;
+}
+class GorgeHome
+{
+	GorgeHome();
+	friend class Bob;
+public:
+	string SittingRoom;
+private:
+	string BedRoom;
+};
+GorgeHome::GorgeHome()
+{
+	SittingRoom = "¬ø√ç√å√º";
+	BedRoom = "√é√î√ä√í";
+}
+class Bob
+{
+	Bob();
+public:
+	void visitting();
+private:
+	
+	GorgeHome* place;
+};
+Bob::Bob()
+{
+	place = new GorgeHome;
+}
+void Bob::visitting()
+{
+	cout << "Bob is visitting GorgeHome " << place->SittingRoom << endl;
+	cout << "Bob is visitting GorgeHome " << place->BedRoom << endl;
+}//
